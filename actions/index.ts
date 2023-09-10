986528b32ecd90a7386bd211c15ff48419e68c4e
Sumeet Wilkhu <sumeet.wilkhu@gmail.com>
2023-09-09 20:08:56 -0400

    Fix the export

--- actions/index.ts
+++ actions/index.ts
@@ -1,1 +1,4 @@
-export actionList = [createNewFileAction1()];
+import { createNewFileAction1 } from './online-file-create';
+export { actionList } = [createNewFileAction1()];
+
+export default actionList;
