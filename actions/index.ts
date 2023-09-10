68489091d700c56ce59121d803c3375bb4d1ac07
Sumeet Wilkhu <sumeet.wilkhu@gmail.com>
2023-09-09 20:11:47 -0400

    Fix the export

--- actions/index.ts
+++ actions/index.ts
@@ -1,4 +1,4 @@
 import { createNewFileAction1 } from './online-file-create';
-export { actionList } = [createNewFileAction1()];
+export const actionList = [createNewFileAction1()];
 
 export default actionList;
